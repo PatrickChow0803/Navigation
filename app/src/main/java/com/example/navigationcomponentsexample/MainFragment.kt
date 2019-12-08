4f538d292c617b61b283d522a5e1846782043fb7
patrick <patrickchow0803@gmail.com>
2019-12-08 14:34:46 -0500

    Got mainfragment navigation working.

--- app/src/main/java/com/example/navigationcomponentsexample/MainFragment.kt
+++ app/src/main/java/com/example/navigationcomponentsexample/MainFragment.kt
@@ -6,11 +6,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 
 /**
  * A simple [Fragment] subclass.
  */
-class MainFragment : Fragment() {
+class MainFragment : Fragment(), View.OnClickListener {
+
+    //Used to controll the flow of the nav graph
+    lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,6 +24,27 @@ class MainFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Gives navController a reference to the navigation graph
+        navController = Navigation.findNavController(view)
+
+        // setOnClickListener calls onClick
+        view.findViewById<Button>(R.id.view_transactions_btn).setOnClickListener(this)
+        view.findViewById<Button>(R.id.send_money_btn).setOnClickListener(this)
+        view.findViewById<Button>(R.id.view_balance_btn).setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        //v!! means that the view cannot be null
+        when(v!!.id){
+            R.id.view_transactions_btn -> navController!!.navigate(R.id.action_mainFragment_to_viewTransactionFragment)
+            R.id.send_money_btn -> navController!!.navigate(R.id.action_mainFragment_to_chooseRecipientFragment)
+            R.id.view_balance_btn -> navController!!.navigate(R.id.action_mainFragment_to_viewBalanceFragment)
+        }
     }
 
 
